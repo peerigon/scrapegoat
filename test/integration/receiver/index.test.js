feab00a577ced646a0b40aa13bb2e6c774d93786
Markus Dobmann <md@noqq.eu>
2015-03-09 12:16:58 +0100

    added caldav request tests

--- test/integration/receiver/index.test.js
+++ test/integration/receiver/index.test.js
@@ -1,20 +1,53 @@
 'use strict';
 
 var expect = require('chai').expect;
-var nock = require('nock');
+var rewire = require('rewire');
+var request = require('request');
 
-describe('receiver', function() {
+var receiver = rewire('../../../lib/caldav/receiver');
 
-    describe('receiver.getChangedCalendar()', function() {
+describe('receiver', function () {
 
-        // TODO: write connection tests (failure, status codes, etc) / content is tested on xmlParser.test.js
+    describe('receiver.getChangedCalendar()', function () {
+
+        before(function () {
+            receiver.__set__('request', function(options, callback) {
+                setImmediate(callback, new Error);
+            });
+        });
+
+        it('should throw an error if there\'s no connection', function() {
+
+            return receiver.getChangedCalendar().catch(function (error) {
+                expect(error).to.be.instanceof(Error);
+            });
+
+        });
+
+        after(function () {
+            receiver.__set__('request', request);
+        });
 
     });
 
-    describe('receiver.getChangedEvents()', function() {
+    describe('receiver.getChangedEvents()', function () {
 
-        // TODO: write connection tests (failure, status codes, etc) / content is tested on xmlParser.test.js
+        before(function() {
+            receiver.__set__('request', function(options, callback) {
+                setImmediate(callback, new Error);
+            });
+        });
 
+        it('should throw an error if there\'s no connection', function() {
+
+            return receiver.getChangedCalendar().catch(function (error) {
+                expect(error).to.be.instanceof(Error);
+            });
+        });
+
+        after(function () {
+            receiver.__set__('request', request);
+        })
     });
 
 });
