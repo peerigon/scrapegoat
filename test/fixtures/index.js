d1b7191b1c2984fa140e61746bfdad9824942ec7
antosan <sandeantony@gmail.com>
2016-12-01 15:15:57 +0100

    Update xml test fixtures

--- test/fixtures/index.js
+++ test/fixtures/index.js
@@ -4,8 +4,7 @@ const fs = require("fs");
 const path = require("path");
 
 const fixtures = {
-    getCtagRequestBody: fs.readFileSync(path.join(__dirname, "/info.request.body.xml"), "utf8"),
-    getCtagResponse: fs.readFileSync(path.join(__dirname, "/info.response.xml"), "utf8"),
+    getCtagResponse: fs.readFileSync(path.join(__dirname, "/ctag.response.xml"), "utf8"),
     getEtagsResponse: fs.readFileSync(path.join(__dirname, "/etags.response.xml"), "utf8"),
     getEventsResponse: fs.readFileSync(path.join(__dirname, "/events.response.xml"), "utf8")
 };
