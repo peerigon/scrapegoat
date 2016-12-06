84d7f5b35b41323681d1de3aa699938598d39de5
Antony Sande <sandeantony@gmail.com>
2016-12-06 14:11:41 +0100

    Update scrapegoat (#16)
    * Wrap constructor in createCalendar function which receives request param
    
    * Update Mocha to v3.1.2
    
    * Add Calendar test
    
    * Add separate test-watch script
    
    * Remove es5 eslint rules in order to use es6 promises
    
    * Add response xml fixture
    
    * Make mocked calendar test pass
    
    * Rewrite pass event to return correct object from xml data
    
    * Update README.md to reflect changes in output
    
    * Add more tests
    
    * Update xml test fixtures
    
    * Add .catch to parseEvents function
    
    * Update calendar tests
    
    * Update getEventsByTime to pass null if end date is not provided
    
    * Update getEventsByTime documentation
    
    * Add missing calendar tests
    
    * Remove .catch from parseEvents function
    
    * Remove .catch from mocha promise tests
    
    * Add request stub() tests
    
    * Make linter happy
    
    * Update to node_version 6 in travis.yml
    
    * Remove old node_modules from travis.yml
    
    * Remove old node_versions from travis.tml
    
    * Remove app.js
    
    * Rewrite Calendar as es6 class

---
+++ test/Calendar.test.js
@@ -0,0 +1,274 @@
+"use strict";
+
+const expect = require("chai").expect;
+const sinon = require("sinon");
+const createCalendar = require("../lib/Calendar");
+const fixtures = require("./fixtures/index");
+
+const CALENDAR_DOMAIN = "http://example.com";
+const CALENDAR_PATH = "/cal.php/calendars/user/calendar_name/";
+const config = {
+    auth: {
+        user: "username",
+        pass: "password"
+    },
+    uri: CALENDAR_DOMAIN + CALENDAR_PATH
+};
+
+describe("Calendar", () => {
+
+    it("should throw if config object is not passed", () => {
+        const response = fixtures.getCtagResponse;
+        const request = Promise.resolve(response);
+        const Calendar = createCalendar(() => request);
+
+        expect(() => new Calendar()).to.throw(Error, /Missing config object/);
+    });
+
+    describe(".getCtag()", () => {
+
+        it("should call request with the correct arguments in the correct order", () => {
+            const response = fixtures.getCtagResponse;
+            const request = sinon.stub();
+            const requestPromise = Promise.resolve(response);
+
+            request.returns(requestPromise);
+
+            const Calendar = createCalendar(request);
+            const calendar = new Calendar(config);
+
+            calendar.getCtag();
+
+            expect(request.callCount).to.equal(1);
+            expect(request.firstCall.args[0]).to.equal(config);
+            expect(request.firstCall.args[1]).to.equal("PROPFIND");
+            expect(request.firstCall.args[2]).to.equal(0);
+        });
+
+        it("should return an object with information about the calendar", () => {
+            const response = fixtures.getCtagResponse;
+            const request = Promise.resolve(response);
+            const Calendar = createCalendar(() => request);
+            const calendar = new Calendar(config);
+
+            return calendar
+            .getCtag()
+            .then((response) => {
+                expect(response).to.have.property("href", CALENDAR_PATH);
+                expect(response).to.have.property("name", "Default calendar");
+                expect(response).to.have.property("ctag", "http://sabre.io/ns/sync/3");
+            });
+        });
+
+    });
+
+    describe(".getEtags()", () => {
+
+        it("should call request with the correct arguments in the correct order", () => {
+            const response = fixtures.getEtagsResponse;
+            const request = sinon.stub();
+            const requestPromise = Promise.resolve(response);
+
+            request.returns(requestPromise);
+
+            const Calendar = createCalendar(request);
+            const calendar = new Calendar(config);
+
+            calendar.getEtags();
+
+            expect(request.callCount).to.equal(1);
+            expect(request.firstCall.args[0]).to.equal(config);
+            expect(request.firstCall.args[1]).to.equal("REPORT");
+            expect(request.firstCall.args[2]).to.equal(1);
+        });
+
+        it("should return an array of object with etags of all events", () => {
+            const response = fixtures.getEtagsResponse;
+            const request = Promise.resolve(response);
+            const Calendar = createCalendar(() => request);
+            const calendar = new Calendar(config);
+
+            return calendar
+            .getEtags()
+            .then((response) => {
+                expect(response).to.be.an("array");
+                expect(response).to.have.lengthOf(3);
+                expect(response[0]).to.have.property("ics");
+                expect(response[0]).to.have.property("etag");
+            });
+        });
+
+    });
+
+    describe(".getEvents()", () => {
+
+        it("should throw if events param is not provided", () => {
+            const response = fixtures.getEventsResponse;
+            const request = Promise.resolve(response);
+            const Calendar = createCalendar(() => request);
+            const calendar = new Calendar(config);
+
+            expect(() => calendar.getEvents()).to.throw(TypeError);
+        });
+
+        it("should throw if events param is not an array", () => {
+            const response = fixtures.getEventsResponse;
+            const request = Promise.resolve(response);
+            const Calendar = createCalendar(() => request);
+            const calendar = new Calendar(config);
+
+            expect(() => calendar.getEvents({})).to.throw(TypeError);
+        });
+
+        it("should throw if events param is an empty array", () => {
+            const response = fixtures.getEventsResponse;
+            const request = Promise.resolve(response);
+            const Calendar = createCalendar(() => request);
+            const calendar = new Calendar(config);
+
+            expect(() => calendar.getEvents([])).to.throw(TypeError);
+        });
+
+        it("should call request with the correct arguments in the correct order", () => {
+            const response = fixtures.getEventsResponse;
+            const request = sinon.stub();
+            const requestPromise = Promise.resolve(response);
+
+            request.returns(requestPromise);
+
+            const Calendar = createCalendar(request);
+            const calendar = new Calendar(config);
+            const events = [
+                { ics: "/calendars/user/calendar_name/nodeschool-augsburg.ics" },
+                { ics: "/calendars/user/calendar_name/sampleevent.ics" },
+                { ics: "/calendars/user/calendar_name/importantevent.ics" }
+            ];
+
+            calendar.getEvents(events);
+
+            expect(request.callCount).to.equal(1);
+            expect(request.firstCall.args[0]).to.equal(config);
+            expect(request.firstCall.args[1]).to.equal("REPORT");
+            expect(request.firstCall.args[2]).to.equal(1);
+        });
+
+        it("should return an array of objects with the passed events", () => {
+            const response = fixtures.getEventsResponse;
+            const request = Promise.resolve(response);
+            const Calendar = createCalendar(() => request);
+            const calendar = new Calendar(config);
+            const events = [
+                { ics: "/calendars/user/calendar_name/nodeschool-augsburg.ics" },
+                { ics: "/calendars/user/calendar_name/sampleevent.ics" },
+                { ics: "/calendars/user/calendar_name/importantevent.ics" }
+            ];
+
+            return calendar
+            .getEvents(events)
+            .then((response) => {
+                expect(response).to.be.an("array");
+                expect(response).to.have.lengthOf(3);
+                expect(response[0]).to.have.property("ics");
+                expect(response[0]).to.have.property("etag");
+                expect(response[0]).to.have.property("data");
+            });
+        });
+
+    });
+
+    describe(".getAllEvents()", () => {
+
+        it("should call request with the correct arguments in the correct order", () => {
+            const response = fixtures.getAllEventsResponse;
+            const request = sinon.stub();
+            const requestPromise = Promise.resolve(response);
+
+            request.returns(requestPromise);
+
+            const Calendar = createCalendar(request);
+            const calendar = new Calendar(config);
+
+            calendar.getAllEvents();
+
+            expect(request.callCount).to.equal(1);
+            expect(request.firstCall.args[0]).to.equal(config);
+            expect(request.firstCall.args[1]).to.equal("REPORT");
+            expect(request.firstCall.args[2]).to.equal(1);
+        });
+
+        it("should return an array of objects with all events in the calendar", () => {
+            const response = fixtures.getAllEventsResponse;
+            const request = Promise.resolve(response);
+            const Calendar = createCalendar(() => request);
+            const calendar = new Calendar(config);
+
+            return calendar
+            .getAllEvents()
+            .then((response) => {
+                expect(response).to.be.an("array");
+                expect(response).to.have.lengthOf(3);
+                expect(response[0]).to.have.property("ics");
+                expect(response[0]).to.have.property("etag");
+                expect(response[0]).to.have.property("data");
+            });
+        });
+
+    });
+
+    describe(".getEventsByTime()", () => {
+
+        it("should call request with the correct arguments in the correct order", () => {
+            const response = fixtures.getEventsByTimeResponse;
+            const request = sinon.stub();
+            const requestPromise = Promise.resolve(response);
+
+            request.returns(requestPromise);
+
+            const Calendar = createCalendar(request);
+            const calendar = new Calendar(config);
+
+            calendar.getEventsByTime("20140101T000000Z", "20151231T235959Z");
+
+            expect(request.callCount).to.equal(1);
+            expect(request.firstCall.args[0]).to.equal(config);
+            expect(request.firstCall.args[1]).to.equal("REPORT");
+            expect(request.firstCall.args[2]).to.equal(1);
+        });
+
+        it("should return an array of objects with all events that occur between start and end dates", () => {
+            const response = fixtures.getEventsByTimeResponse;
+            const request = Promise.resolve(response);
+            const Calendar = createCalendar(() => request);
+            const calendar = new Calendar(config);
+
+            return calendar
+            .getEventsByTime("20140101T000000Z", "20151231T235959Z")
+            .then((response) => {
+                expect(response).to.be.an("array");
+                expect(response).to.have.lengthOf(2);
+                expect(response[0]).to.have.property("ics");
+                expect(response[0]).to.have.property("etag");
+                expect(response[0]).to.have.property("data");
+            });
+        });
+
+        it("should return an array of objects with all upcoming events from today if start and end are left out", () => {
+            const response = fixtures.getFutureEventsResponse;
+            const request = Promise.resolve(response);
+            const Calendar = createCalendar(() => request);
+            const calendar = new Calendar(config);
+
+            return calendar
+            .getEventsByTime()
+            .then((response) => {
+                expect(response).to.be.an("array");
+                expect(response).to.have.lengthOf(1);
+                expect(response[0]).to.have.property("ics");
+                expect(response[0]).to.have.property("etag");
+                expect(response[0]).to.have.property("data");
+            });
+        });
+
+    });
+
+});
---
+++ test/fixtures/index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const fs = require("fs");
+const path = require("path");
+
+const fixtures = {
+    getCtagResponse: fs.readFileSync(path.join(__dirname, "/ctag.response.xml"), "utf8"),
+    getEtagsResponse: fs.readFileSync(path.join(__dirname, "/etags.response.xml"), "utf8"),
+    getEventsResponse: fs.readFileSync(path.join(__dirname, "/events.response.xml"), "utf8"),
+    getAllEventsResponse: fs.readFileSync(path.join(__dirname, "/eventsAll.response.xml"), "utf8"),
+    getEventsByTimeResponse: fs.readFileSync(path.join(__dirname, "/eventsByTime.response.xml"), "utf8"),
+    getFutureEventsResponse: fs.readFileSync(path.join(__dirname, "/eventsFuture.response.xml"), "utf8")
+};
+
+module.exports = fixtures;
