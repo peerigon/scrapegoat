78e86eca5cc83e038cb333868bbdce71bac47e4f
Tanner Hoisington <hoisington.tanner@gmail.com>
2018-10-08 14:59:00 +0200

    feat: adds test for request function

---
+++ test/integration/request/request.test.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const nock = require("nock");
+const expect = require("chai").expect;
+const createCalendar = require("../../../lib/Calendar");
+const request = require("../../../lib/request");
+const xml = require("../../../lib/xml");
+
+const CALENDAR_DOMAIN = "http://example.com";
+const CALENDAR_PATH = "/cal.php/calendars/user/calendar_name/";
+const config = {
+    auth: {
+        user: "username",
+        pass: "password"
+    },
+    uri: CALENDAR_DOMAIN + CALENDAR_PATH
+};
+
+describe("Request", () => {
+    it("should call request with the correct headers", done => {
+        const calendarRequest = nock(CALENDAR_DOMAIN, {
+            reqheaders: {
+                "Content-length": xml.calendarCtag.length,
+                Depth: 0,
+                host: "example.com"
+            }
+        })
+            .intercept(CALENDAR_PATH, "PROPFIND")
+            .reply(200, {});
+
+        const Calendar = createCalendar(request);
+        const calendar = new Calendar(config);
+
+        calendar.getCtag().catch(() => {
+            // There will be an error because of the code in request we arent testing for
+            expect(calendarRequest.isDone()).to.equal(true);
+            done();
+        });
+    });
+});
