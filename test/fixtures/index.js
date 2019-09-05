493c7a504a5a02544ccbe0cfe8adc2939474d401
Martin Hofmann <martin.hofmann@peerigon.com>
2019-09-05 18:17:53 +0200

    test: add fixtures for iCloud calendar style responses

--- test/fixtures/index.js
+++ test/fixtures/index.js
@@ -5,9 +5,12 @@ const path = require("path");
 
 const fixtures = {
     getCtagResponse: fs.readFileSync(path.join(__dirname, "/ctag.response.xml"), "utf8"),
+    getCtagNoNamespaceResponse: fs.readFileSync(path.join(__dirname, "/ctagNoNamespace.response.xml"), "utf8"),
     getEtagsResponse: fs.readFileSync(path.join(__dirname, "/etags.response.xml"), "utf8"),
+    getEtagsNoNamespaceResponse: fs.readFileSync(path.join(__dirname, "/etagsNoNamespace.response.xml"), "utf8"),
     getEventsResponse: fs.readFileSync(path.join(__dirname, "/events.response.xml"), "utf8"),
     getAllEventsResponse: fs.readFileSync(path.join(__dirname, "/eventsAll.response.xml"), "utf8"),
+    getAllEventsNoNamespaceResponse: fs.readFileSync(path.join(__dirname, "/eventsAllNoNamespace.response.xml"), "utf8"),
     getEventsByTimeResponse: fs.readFileSync(path.join(__dirname, "/eventsByTime.response.xml"), "utf8"),
     getFutureEventsResponse: fs.readFileSync(path.join(__dirname, "/eventsFuture.response.xml"), "utf8")
 };
